4f7a4eb195825b58f4139cec160aa6c4949e285b
Muhamad Farid <muhamadfaridalmubarok2002@gmail.com>
2026-02-23 09:55:01 +0800

    Upload Ulang

---
+++ kelas/pertemuan-1/materi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    string nama;
+    cout << "Hello World! " << endl;
+    cout << "Belajar C++ Sangat Seru 😄" << endl;
+    cout << "Masukkan Nama: ";
+    getline(cin, nama);
+
+    return 0;
+}
